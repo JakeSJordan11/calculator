c9a7e5a447e4dc75ea93c1b7ced82ff6ec81a2d1
jake <jakesjordan11@gmail.com>
2019-11-16 07:13:22 -0700

    refactoring and added comments

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React, { useState } from "react";
-import Grid from "./Components/StyledComponents/Grid";
-import Button from "./Components/Button";
-import Input from "./Components/Input";
-import ClearButton from "./Components/ClearButton";
 import * as math from "mathjs";
+import React, { useState } from "react";
+import { Button, ClearButton, Input } from "./Components";
+import Grid from "./Components/StyledComponents/Grid";
 
-function App(props) {
+function App() {
   const [input, setInput] = useState("");
+  const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+  /*creates a function that uses an array to see 
+  if a given string ends with any item in that array */
   const endsWithAny = (suffixes, string) => {
     return suffixes.some(function(suffix) {
       return string.endsWith(suffix);
     });
   };
 
-  const addToInput = val => {
-    setInput(input + val);
-  };
-
+  /*   when equals is clicked it only evaluates the input field
+  if the last item is a number and the input field is not blank. */
   const handleEqual = () => {
-    if (
-      input.length > 1 &&
-      endsWithAny(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], input) ===
-        true
-    ) {
+    if (input.length > 1 && endsWithAny(numbers, input) === true) {
       setInput(math.evaluate(input));
     }
+  };
+
+  // adds clicked button to the input field.
+  const addToInput = val => {
+    setInput(input + val);
   };
 
   return (
---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+import Button from "./Button";
+import ClearButton from "./ClearButton";
+import Input from "./Input";
+
+export { Button, ClearButton, Input };
